3209cb12ef388d60c1ab288dd7405d43390c350a
Andrew Ayegh <andrewayegh2004@gmail.com>
2023-01-10 13:56:26 +0100

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -47,14 +47,14 @@ while game_is_on:
     
     # detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < - 290 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
     
     # detect collision with tail
     for segment in snake.segments:
         if segment != snake.head:
             if snake.head.distance(segment) < 10:
-                game_is_on = False
-                score.game_over()
+                score.reset()
+                snake.reset()
         
 screen.exitonclick()
